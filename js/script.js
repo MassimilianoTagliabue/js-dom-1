dd0903a982581af876a8fdb8a1eedcf184fcab7a
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-11-07 15:37:57 +0100

    feat:bonus accendi spegni

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
-const lampElement = document.getElementById("lamp");
-console.log(lampElement);
 
+//assegno a lampElement l'elemento con l'id lamp
+const lampElement = document.getElementById("lamp");
+
+//assegno a btnElement l'elemento con l'id lamp-btn
 const btnElement = document.getElementById("lamp-btn")
 
-console.log(btnElement);
 
-btnElement.addEventListener("click", function (){
-    
-    lampElement.src = "./img/yellow_lamp.png";
-    lampElement.alt = "lampada accesa";
 
-    
-    
+
+
+btnElement.addEventListener("click", function () {
 
     
+    //controllo se l'immagine ha la classe white
+    if (lampElement.classList.contains("white")) {
+
+        lampElement.classList.add("yellow"); //aggiungo la classe yellow
+        lampElement.classList.remove("white");  //rimuovo la classe white
+
+        lampElement.src = "./img/yellow_lamp.png"; //metto l'immagine della lampada accesa
+        lampElement.alt = "lampada accesa";
+
+       
+
+    } else {
+
+        lampElement.classList.add("white");     //aggiungo la classe white
+        lampElement.classList.remove("yellow"); //rimuovo la classe yellow
+
+        lampElement.src = "./img/white_lamp.png";   //metto l'immagine della lampada spenta
+        lampElement.alt = "lampada spenta";
+
+
+    }
+
+
+
 })
+
+
+
+
